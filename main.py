cc088f476692c64f301c3378ec4ca5c3ccbf6f64
SoulzzDev <aatuheinanen@gmail.com>
2021-05-18 08:01:51 +0300

    file reader is working and image is resizable

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from PIL import Image, ImageTk
 def OpenFile():
     filepath = filedialog.askopenfilename()
     file = Image.open(filepath)
-    render = ImageTk.PhotoImage(file)
+    file_new = file.resize((400, 400))
+    render = ImageTk.PhotoImage(file_new)
     img = Label(image=render)
     img.image = render
     img.place(x=0, y=0)
 
 
 window = Tk()
+window.geometry("400x400")
 
 button = Button(text='Open', command=OpenFile)
 button.pack()
