f4e0430e7b8aefa1597dd24fca22b2e7006e72ba
SoulzzDev <aatuheinanen@gmail.com>
2021-05-18 16:59:17 +0300

    gets the text from the entrybox

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
+window = Tk()
+window.geometry("405x450")
+
+## File reader
 def OpenFile():
     filepath = filedialog.askopenfilename()
     file = Image.open(filepath)
@@ -11,13 +15,18 @@ def OpenFile():
     img.image = render
     img.place(x=0, y=0)
 
-def TextLabel():
-    entryBox = Entry()
-    entryBox.pack()
-
-window = Tk()
-window.geometry("405x450")
 button = Button(text='Open', command=OpenFile)
 button.place(x=190, y=410)
-TextLabel()
+entryBox = Entry()
+entryBox.pack()
+
+## gets the text from the entrybox
+def TextLabel():
+    label = Label(text=entryBox.get())
+    label.pack()
+    label.place(x=190, y=100)
+
+textButton = Button(text="Enter", command=TextLabel())
+textButton.pack()
+
 window.mainloop()
